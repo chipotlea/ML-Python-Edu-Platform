80ac817e2bde92be6019ab25b914467cc97a3f6f
chipotlea <133628124+chipotlea@users.noreply.github.com>
2023-01-02 20:19:57 +0000

    typo in readme

--- utils/cookie.ts
+++ utils/cookie.ts
@@ -14,4 +14,12 @@ export const getCookie = (name: string) => {
   var ca = document.cookie.split(";");
   for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
+
+export const eraseCookie = (name: string) => {
+  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+};
